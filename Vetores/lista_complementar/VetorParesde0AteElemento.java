d3a3dbc4b8f5b16c04c55f1e668122fcbed3e07f
Virowlet <vitor06mococa@gmail.com>
2025-11-10 20:12:05 -0300

    Atividade TPA

--- Vetores/lista_complementar/VetorParesde0AteElemento.java
+++ Vetores/lista_complementar/VetorParesde0AteElemento.java
@@ -15,6 +15,7 @@ public class VetorParesde0AteElemento {
 			System.out.println("Insira o " + (i+1) + "° valor do vetor A:");
 			a[i] = ler.nextInt();
 		}
+		System.out.println("Os divisores de cada elemento são:");
 		for(i=0;i<TAM;i++) {
 			System.out.print(a[i] + "( ");
 			for(j=0;j<=a[i];j++) {
